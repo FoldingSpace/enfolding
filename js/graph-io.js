5b10dfce0a9e0a374e0deb99627f8e15ac7b2939
Luke Bergmann <geoglrb@uw.edu>
2018-06-04 17:13:53 -0700

    fixing XML

--- js/graph-io.js
+++ js/graph-io.js
@@ -3,15 +3,15 @@
 
 function GraphXMLfromString(xmlString){
   //
-  console.log("XML data seen: "+xmlString)
+  console.log("XML data seen: "+xmlString);
 
   parser = new DOMParser();
-  xmlDoc = parser.parseFromString(xmlString,"text/xml")
+  xmlDoc = parser.parseFromString(xmlString,"text/xml");
 
   // get the keys
   var keychildren = xmlDoc.getElementsByTagName('key');
-  console.log('keychildren has this number of elements: '+keychildren.length)
-  console.log(keychildren)
+  console.log('keychildren has this number of elements: '+keychildren.length);
+  console.log(keychildren);
 
   var keyid, fortype, attrtype;
   var keys = {};
@@ -19,24 +19,28 @@ function GraphXMLfromString(xmlString){
     keyid = keychildren[i].getAttribute('id');
     fortype = keychildren[i].getAttribute('for');
     attrname = keychildren[i].getAttribute('attr.name');
-    keys[keyid] = {for: fortype, attrname: attrname}
+    keys[keyid] = {for: fortype, attrname: attrname};
     console.log(keyid + ', ' + fortype + ', ' + attrname);
-  }
+  };
 
   // parse nodes
 
   var nodechildren = xmlDoc.getElementsByTagName('node');
   var datachildren;
+  var nodes = {};
   for (var currnode = 0; currnode < nodechildren.length; currnode++) {
+    nodeid = nodechildren[currnode].getAttribute('id');
     datachildren = nodechildren[currnode].getElementsByTagName('data');
+    var current_node = {};
     for (var currdata = 0; currdata < datachildren.length; currdata++) {
       keyid = datachildren[currdata].getAttribute('key');
       keycontent = datachildren[currdata].childNodes[0].nodeValue;
-      /*console.log("Node " + currnode + " has key " + keyid + ' of ' + keycontent);*/
-      console.log("Node " + currnode + " has key " + keys[keyid].attrname + ' of ' + keycontent)
+      console.log("Node " + nodeid + " has key " + keys[keyid].attrname + ' of ' + keycontent);
+      current_node = current_node + {keys[keyid].attrname: keycontent};
     }
+    nodes = nodes + current_node;
   }
-
+  console.log(nodes);
 
   // add the nodes to enfolding
 
